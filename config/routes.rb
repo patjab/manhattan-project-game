961b78d77440cd1fbdf5163dbbbdaa4a71857bb8
github username <github email address>
2018-06-21 11:11:51 -0400

    enabled the ability to change nation name; added indication of no matches presently ongoing

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'users/', to: 'users#index', as: 'users'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+
+  patch 'users/:id', to: 'users#update'
+
   get 'users/:id', to: 'users#show', as: 'user'
   post 'users/', to: 'users#create'
 
