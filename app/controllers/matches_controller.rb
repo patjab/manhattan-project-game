ae38e2ba371738b7224253f254b398066a10c8d6
github username <github email address>
2018-06-26 07:27:25 -0400

    cleaned up code again to be more object oriented

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,6 @@
 class MatchesController < ApplicationController
   before_action :authorized
-  before_action :establish_turn, only: [:show, :evaluate]
+  before_action :preserve_turn_statefulness, only: [:show, :evaluate, :espionage_on]
 
   def new
     @match = Match.new
@@ -14,10 +14,9 @@ class MatchesController < ApplicationController
   end
 
   def show
-    check_for_a_winner
-    @correctness_msg = nil
+    @match.winner_of_match ? (render :'finished.html') : nil
     @team = current_user.team_roaster(@match)
-    @espionage_on_opponent = @match.espionage_on_you?(opponent)
+    @espionage_on_opponent = @match.espionage_on_you?(@opponent)
     @match.update(current_question_id: @random_q.id)
     @message = Stage.as_readable_array[current_user.total_team_members(@match)]
   end
@@ -25,7 +24,7 @@ class MatchesController < ApplicationController
   def evaluate
     @message = Stage.as_readable_array[current_user.total_team_members(@match)]
     @correctness_msg = (params[:question][:option] == @random_q.answer ? current_user.process_correct_response(@match, @random_q) : current_user.process_wrong_response(@match, @random_q))
-    @match.update(current_turn_user_id: opponent.id, current_question_id: nil)
+    @match.update(current_turn_user_id: @opponent.id, current_question_id: nil)
     @team = current_user.team_roaster(@match)
     render :'show.html'
   end
@@ -45,7 +44,7 @@ class MatchesController < ApplicationController
 
   def espionage_on
     @match = Match.find(params[:id])
-    @match.is_challenger?(opponent) ? (@match.espionage_on_challenger = true) : (@match.espionage_on_challenged = true)
+    @match.is_challenger?(@opponent) ? (@match.espionage_on_challenger = true) : (@match.espionage_on_challenged = true)
     @match.save
     redirect_to match_path(@match)
   end
@@ -57,20 +56,12 @@ class MatchesController < ApplicationController
   end
 
   private
-  def check_for_a_winner
-    @winner = @match.winner_of_match
-    @loser = @match.loser_of_match
-    @winner ? (render :'finished.html') : nil
-  end
-
-  def establish_turn
+  def preserve_turn_statefulness
     @match = Match.find(params[:id])
     @random_q = @match.find_or_generate_random_question(current_user)
     @espionage = @match.espionage_ability?(current_user)
     @espionage_on_you = @match.espionage_on_you?(current_user)
-  end
-
-  def opponent
-    @match.the_other_user(current_user)
+    @opponent = @match.the_other_user(current_user)
+    @correctness_msg = nil
   end
 end
